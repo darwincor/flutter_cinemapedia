8f753de0b3278b76c6cacf406799e5b94b1b7b39
Darwin <darwinpatricio_8@hotmail.com>
2024-10-22 13:42:21 +0200

    Configure .env file with flutter_dontenv

--- lib/presentation/screens/video/home_screen.dart
+++ lib/presentation/screens/video/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const HomeScreen());
@@ -9,9 +10,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Material App',
-      home: Placeholder()
+    return Scaffold(
+      body: Center(child: Text(Environment.theMovieDbKey)),
     );
   }
 }
