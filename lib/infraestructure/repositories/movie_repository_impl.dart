5d7bb8454c393fd4177fb6f0198b5393a9f69a66
Darwin <darwinpatricio_8@hotmail.com>
2024-10-22 17:16:03 +0200

    Get now playing videos

---
+++ lib/infraestructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,18 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+import 'package:cinemapedia/infraestructure/datasources/movies_datasource.dart';
+
+
+class MovieRepositoryImpl extends MoviesRepository {
+
+  final MoviesDatasource datasource;
+  MovieRepositoryImpl(this.datasource);
+
+  
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+
+
+}
