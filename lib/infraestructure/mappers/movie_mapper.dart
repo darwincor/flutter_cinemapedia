371c5a40313d29db67cd8a116d878bfb61509917
Darwin <darwinpatricio_8@hotmail.com>
2024-10-23 16:19:58 +0200

    Implement searcher

--- lib/infraestructure/mappers/movie_mapper.dart
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -16,15 +16,15 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
         ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
-        : 'no-poster',
-      releaseDate: moviedb.releaseDate,
+        : 'https://www.movienewz.com/img/films/poster-holder.jpg',
+      releaseDate: moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
       title: moviedb.title,
       video: moviedb.video,
       voteAverage: moviedb.voteAverage,
       voteCount: moviedb.voteCount
     );
 
-  static Movie movieDetailsToEntity( MovieDetails moviedb ) => Movie(
+    static Movie movieDetailsToEntity( MovieDetails moviedb ) => Movie(
       adult: moviedb.adult,
       backdropPath: (moviedb.backdropPath != '') 
         ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
@@ -44,4 +44,5 @@ class MovieMapper {
       voteAverage: moviedb.voteAverage,
       voteCount: moviedb.voteCount
     );
+
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,11 +1,16 @@
 
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/search/search_movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
@@ -24,8 +29,24 @@ class CustomAppbar extends StatelessWidget {
       
               const Spacer(),
       
-              IconButton(onPressed: (){
-      
+              IconButton(onPressed: () {
+
+                final searchedMovies = ref.read( searchedMoviesProvider );
+                final searchQuery = ref.read(searchQueryProvider);
+                
+                showSearch<Movie?>(
+                  query: searchQuery,
+                  context: context, 
+                  delegate: SearchMovieDelegate(
+                    initialMovies: searchedMovies,
+                    searchMovies: ref.read( searchedMoviesProvider.notifier ).searchMoviesByQuery
+                  )
+                ).then((movie) {
+                  if ( movie == null ) return;
+
+                  context.push('/movie/${ movie.id }');
+                });
+
               }, 
               icon: const Icon(Icons.search)
               )
